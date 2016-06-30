a6685a48f0929967ed9283d8fcac0a48ae443ff7
wesbasinger <wbasinger@villagetechschools.org>
2016-06-29 20:21:55 -0500

    Sockets can connect and disconnect.

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', socket => {
+	console.log('a user connected.');
+	
+	socket.on('disconnect', () => {
+		console.log('a user disconnected.');
+	});
+});
+
 
 http.listen(3000, () => {
 	console.log('listening on port 3000...');
