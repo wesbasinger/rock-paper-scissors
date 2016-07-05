d4743b23a71d908a6d4925fdcd6a0f49d9b3500c
wesbasinger <wbasinger@villagetechschools.org>
2016-07-05 09:40:11 -0500

    Deleted vestigal remnats and compacted.

--- public/script.js
+++ public/script.js
@@ -54,89 +54,14 @@ var Game = React.createClass({
 				<h1>Messages: {this.state.playerPosition} </h1>
 				<h1>Player One: {this.state.playerOneChoice || "Not played yet."}</h1>
 				<h1>Player Two: {this.state.playerTwoChoice || "Not played yet."}</h1>
-				<input
-					type="radio"
-					name="uplay"
-					value="rock"
-					onChange={this.handleChange}/> Rock <br />
-				<input
-					type="radio"
-					name="uplay"
-					value="paper"
-					onChange={this.handleChange}/> Paper <br />
-				<input
-					type="radio"
-					name="uplay"
-					value="scissors"
-					onChange={this.handleChange}/> Scissors <br />
+				<input type="radio" name="uplay" value="rock" onChange={this.handleChange}/> Rock <br />
+				<input type="radio" name="uplay" value="paper" onChange={this.handleChange}/> Paper <br />
+				<input type="radio" name="uplay" value="scissors" onChange={this.handleChange}/> Scissors <br />
 				<button onClick={this.handlePlay}>Play</button>
-				<h1>Result: {this.state.result || "No result yet!"}</h1>
+				<h1>Result: {this.state.result}</h1>
 			</div>
 		)
 	}
 })
 
 ReactDOM.render(<Game />, document.getElementById('react-container'));
-
-/* OLD SCRIPT
-var gamePacket = {};
-
-socket.on('welcome', msg => {
-	$('#messages').text(msg.msg);
-	var radios = document.getElementsByName('uplay');
-	for (radio in radios) {
-		radios[radio].onclick = function() {
-			if ($('#messages').text() == "You are player 1!!!") {
-				$('#p1').text(this.value);
-				gamePacket.player1 = this.value;
-			} else if ($('#messages').text() == "You are player 2!!!") {
-				$('#p2').text(this.value);
-				gamePacket.player2 = this.value;
-			}
-			socket.emit('played', {gamePacket:gamePacket});
-		}
-	}
-});
-
-
-
-socket.on('gameOver', data => {
-	var newButton = $('<button id="startgame"></button').text("Start New Game");
-	$('#newgame').append(newButton);
-	$('#startgame').click(() => {
-		$('#p1').text("");
-		$('#p2').text("");
-		$('#result').text("");
-		$('input[name="uplay"]').prop('checked', false);
-		$('#startgame').remove();
-		socket.emit('newGame', {});
-	});
-	if (data.msg === "Tie game!") {
-		$('#result').text("Tie Game!!!");
-		if ($('#p1').text() === "") {
-			$('#p1').text(data.game.player1);
-		} else {
-			$('#p2').text(data.game.player2);
-		}
-	} else {
-		if ($('#p1').text() === "") { // I must be player 2 then
-			$('#p1').text(data.game.player1);
-			if ($('#p2').text() === data.msg) {
-				$('#result').text("You win!!!");
-			} else {
-				$('#result').text("You lose!!!");
-			}
-		} else if ($('#p2').text() === "") { // I must be player 1 then
-			$('#p2').text(data.game.player2);
-			if ($('#p1').text() === data.msg) {
-				$('#result').text("You win!!!");
-			} else {
-				$('#result').text("You lose!!!");
-			}
-		} else {
-			console.error("Something bad happened...")
-		}
-
-	}
-});
-*/
